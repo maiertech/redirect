507c762e3ae78d58f12d35041e71fa7194a5309d
Thilo Maier <git@maier.asia>
2026-01-18 16:21:03 +0100

    Switch back to redirects only Fixes #28

--- src/lib/assets/utils.ts
+++
@@ -1,7 +0,0 @@
-export function getRedirectUrl(url: URL): string {
-	const redirectUrl = new URL(url);
-	redirectUrl.protocol = 'https:';
-	redirectUrl.hostname = 'maier.tech';
-	redirectUrl.port = '';
-	return redirectUrl.toString();
-}
