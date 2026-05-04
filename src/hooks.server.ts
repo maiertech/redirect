a1130446f52da2352e5ab9b0d202de03e2230146
Thilo Maier <git@maier.asia>
2026-05-04 21:51:22 +0200

    Redirect `maier.social` to `maier.tech` (#50)
    * Redirect `maier.social` to `maier.tech`
    Fixes #49
    
    * Fix `pnpm-workspace.yaml`

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,12 @@ import { redirect, error } from '@sveltejs/kit';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event }) => {
-	// Redirect www.maier.tech to maier.tech with a 301 "Moved Permanently" status.
+	// Redirect any path under maier.social to maier.tech (root).
+	if (event.url.hostname === 'maier.social') {
+		redirect(301, 'https://maier.tech/');
+	}
+
+	// Redirect www.maier.tech to maier.tech and preserve path and query parameters.
 	if (event.url.hostname === 'www.maier.tech') {
 		redirect(301, `https://maier.tech${event.url.pathname}${event.url.search}`);
 	}
